d42f6df65cedd2236276f5af342431ba51b18041
Salman Ayaz <salmanayazz@gmail.com>
2024-10-06 10:30:53 -0700

    added auth to image get req

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// import React from "react";
+import React from "react";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
 type PostProps = {
     username: string;
     profilePicture: string;
@@ -10,6 +13,33 @@ type PostProps = {
 };
 
 const Post: React.FC<PostProps> = ({ username, profilePicture, imageId, caption, title, date }) => {
+    const {getToken} = useAuth();
+
+    const [image, setImage] = React.useState<File | null>(null);
+
+    async function fetchPostImage(imageId: string) {
+        const token = await getToken();
+       const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts/image/${imageId}`, 
+            {headers: 
+                {Authorization: `${token}`}
+            }
+        );
+
+        const blob = await response.blob();
+
+        setImage(new File([blob], imageId, {
+            type: blob.type,
+        }));
+        
+    }
+
+    useEffect (() => {
+        if (imageId) {
+            fetchPostImage(imageId);
+        }
+    }, [imageId]);
+
+    
     return(
         <div className="flex justify-center my-10">
             <div className="border-1 p-4 rounded-lg shadow-md w-[600px] w-[90%] max-w-[600px] bg-white">
@@ -21,7 +51,7 @@ const Post: React.FC<PostProps> = ({ username, profilePicture, imageId, caption,
                 <div className="body">
                     {imageId ? (
                         <>
-                            <img className="w-full mt-[16px] mr-[12px]" src={`${import.meta.env.VITE_SERVER_URL}/posts/image/${imageId}`} alt="Content" />
+                            {image && <img className="w-full mt-[16px] mr-[12px]" src={URL.createObjectURL(image)} alt="Content" />}
                             <h3 className="my-[8px] text-sm font-semibold">{title}</h3>
                             <p className="text-xs text-[#555]">{caption}</p>
                             <p className="text-xs text-[#888]">{date.split('T')[0]}</p>
